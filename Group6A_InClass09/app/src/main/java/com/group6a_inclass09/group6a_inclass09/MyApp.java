a501055faf21699a43c3be1a4baefc43a11b8248
Arunkumar <b.arun410@gmail.com>
2015-11-09 18:55:10 -0500

    With setup for Parse

---
+++ Group6A_InClass09/app/src/main/java/com/group6a_inclass09/group6a_inclass09/MyApp.java
@@ -0,0 +1,16 @@
+package com.group6a_inclass09.group6a_inclass09;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by Arunkumar's on 11/9/2015.
+ */
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "wWu9Arz2YhejpA42tEwdbbJMErPdhZ2bZv1aFoon", "nagmSG7im7nI7maMz66aMmgiGbtHPfdTQeeCxkDA");
+    }
+}
